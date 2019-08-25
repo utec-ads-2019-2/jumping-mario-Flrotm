a5877c6cd03f1d293c2b5f2b23acadeccce1c37b
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-08-24 22:19:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
-}
 
+
+int main() {
+    int tests=0,walls=0;
+
+
+    cin>>tests;
+
+    for(int i=0;i<tests;i++){
+        cin>>walls;
+        int temp[walls];
+        int low=0,high=0;
+        for(int j=1;j<=walls;j++){
+            cin>>temp[j];
+        }
+        for(int h=1;h<walls;h++){
+            if (temp[h]<temp[h+1]){
+                high++;
+            }
+            if(temp[h]>temp[h+1]){
+                low++;
+
+            }
+
+        }
+
+        cout<<"Case "<<i+1<<": "<<high<<" "<<low<<endl;
+
+    }
+
+
+}
